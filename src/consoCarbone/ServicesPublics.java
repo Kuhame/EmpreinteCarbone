653fdb77837bdbd8ee2e3d2d58cb81112e6daea2
yusa-ai <ryan.malonzo@outlook.com>
2022-12-18 22:49:43 +0100

    Fix typo

--- src/consoCarbone/ServicesPublics.java
+++ src/consoCarbone/ServicesPublics.java
@@ -30,7 +30,7 @@ public class ServicesPublics extends ConsoCarbone {
 
     @Override
     public String recommandation() {
-        return "Vous pourriez demander à votre collectivité de réduire, notamment, le temps durant lequel sont allumées " +
+        return "Vous pourriez demander à votre collectivité de réduire, notamment, le temps durant lequel sont allumés " +
                 "les éclairages des rues ou des commerces, particulièrement le jour et la nuit après fermeture.\n" +
                 "Attention : la nécessaire réduction des émissions du service public ne signifie pas moins de service public, au contraire !";
     }
